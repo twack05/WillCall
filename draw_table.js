63ae4b41bfd8c6ca646a4808229c824410fea870
Todd Wacker <twac05@yahoo.com>
2014-08-27 13:42:51 -0500

    Add table

---
+++ draw_table.js
@@ -0,0 +1,9 @@
+var tableData = "";
+for (var i = 0; i < data.report_data.length; i ++) {
+  tableData += "<tr><td>"+data.report_data[i].pickup_first_name+" "+data.report_data[i].pickup_last_name+"</td>"+
+  "<td>"+data.report_data[i].section+"</td>"+
+  "<td>"+data.report_data[i].ticket_count+"</td>"+
+  "<td>"+data.report_data[i].group_code+"</td></tr>";
+};
+
+$('#tableData').html(tableData);
