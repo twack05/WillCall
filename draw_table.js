90598c29ce50bbd99a08386c5e05ea83cf87f9de
Todd Wacker <twac05@yahoo.com>
2014-08-27 16:11:51 -0500

    Show tickets details on button click

--- draw_table.js
+++ draw_table.js
@@ -3,7 +3,35 @@ for (var i = 0; i < data.report_data.length; i ++) {
   tableData += "<tr><td>"+data.report_data[i].pickup_first_name+" "+data.report_data[i].pickup_last_name+"</td>"+
   "<td>"+data.report_data[i].section+"</td>"+
   "<td>"+data.report_data[i].ticket_count+"</td>"+
-  "<td>"+data.report_data[i].group_code+"</td></tr>";
+  "<td>"+data.report_data[i].group_code+"</td>"+
+  "<td><input type='button' value='View Tickets' id='ticket"+i+"'></input></td></tr>";
+
+  $('#tableData').html(tableData);
+
+  var ticketData;
+   for (ticket in data.report_data[i].tickets) {
+      ticketData += "<tr class='ticket"+i+" hidden'><td>"+data.report_data[i].tickets[ticket].event_name+"</td>"+
+            "<td>"+data.report_data[i].tickets[ticket].title+"</td>"+
+            "<td>"+data.report_data[i].tickets[ticket].status+"</td></tr>";
+
+            $('.ticketData').html(ticketData);
+        };
 };
 
-$('#tableData').html(tableData);
+    for (var i = 0; i < data.report_data.length; i ++) {
+        $(".ticket"+i+"").hide();
+          $("#ticket"+i+"").on('click', function(e){
+            e.preventDefault();
+            $('.ticketData').show();
+            $(".hidden").hide();
+            $('#ticketHead').show();
+            $("."+this.id+"").show();
+          });
+    };
+
+$(document).ready(function(){
+  $('#ticketHead').hide();
+  $('.ticketData').hide();
+});
+
+
